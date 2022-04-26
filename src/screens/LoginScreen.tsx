39b242362cec1923df45a5e8dd8f6fd49879186b
Oscar Chan <oscarchan@asl.com.hk>
2022-04-26 12:03:22 +0800

    feature: finish login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,14 +1,140 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 
-const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+// forms
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// navigation
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { AppStackParamList } from '../App';
+
+const schema = yup.object().shape({
+  username: yup.string().required('Please enter username'),
+  password: yup.string().required('Please enter password'),
+});
+
+interface Props {
+  navigation: NativeStackNavigationProp<AppStackParamList, 'Login'>;
 }
 
-export default LoginScreen
+const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onTouched',
+  });
 
-const styles = StyleSheet.create({})
+  const usernameRef = useRef<TextInput>(null);
+  const passwordRef = useRef<TextInput>(null);
+  const focusUsername = () => usernameRef.current?.focus();
+  const focusPassword = () => passwordRef.current?.focus();
+
+  const onSubmit = handleSubmit(data => {
+    navigation.navigate('CategoryList');
+    reset();
+  });
+
+  return (
+    <View style={styles.view}>
+      <Controller
+        control={control}
+        name="username"
+        defaultValue=""
+        render={({ field }) => {
+          const { onChange, onBlur, value } = field;
+          const error = errors.username?.message;
+          return (
+            <TouchableOpacity
+              style={styles.inputContainer}
+              onPress={focusUsername}
+              activeOpacity={1}>
+              <Text style={styles.label}>Username</Text>
+              <TextInput
+                ref={usernameRef}
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={focusPassword}></TextInput>
+              {error && <Text style={styles.error}>{error}</Text>}
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <Controller
+        control={control}
+        name="password"
+        defaultValue=""
+        render={({ field }) => {
+          const { onChange, onBlur, value } = field;
+          const error = errors.password?.message;
+          return (
+            <TouchableOpacity
+              style={styles.inputContainer}
+              onPress={focusPassword}
+              activeOpacity={1}>
+              <Text style={styles.label}>Password</Text>
+              <TextInput
+                ref={passwordRef}
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}></TextInput>
+              {error && <Text style={styles.error}>{error}</Text>}
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <TouchableOpacity style={styles.button} onPress={onSubmit}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: 'orange',
+  },
+  inputContainer: {
+    marginBottom: 10,
+    // backgroundColor: 'yellow',
+  },
+  label: {
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    // backgroundColor: 'blue',
+  },
+  error: {
+    color: 'red',
+    paddingTop: 10,
+    // backgroundColor: 'pink',
+  },
+  button: {
+    height: 40,
+    padding: 10,
+    marginTop: 10,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+  },
+});

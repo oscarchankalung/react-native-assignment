2834ba55c3c3ba15a2aad1a11eb71b46b0f04e66
Oscar Chan <oscarchan@asl.com.hk>
2022-04-26 12:00:14 +0800

    feature: provide store and add navigation type

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// screens
 import LoginScreen from './screens/LoginScreen';
 import CategoryListScreen from './screens/CategoryListScreen';
 import ItemDetailScreen from './screens/ItemDetailScreen';
 import ItemListScreen from './screens/ItemListScreen';
 
-const Stack = createNativeStackNavigator();
+// redux
+import store from './store';
+import {Provider} from 'react-redux';
+
+export type AppStackParamList = {
+  Login: undefined;
+  CategoryList: undefined;
+  ItemDetail: undefined;
+  ItemList: undefined;
+};
+
+const Stack = createNativeStackNavigator<AppStackParamList>();
 
 const App = () => {
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="CategoryList" component={CategoryListScreen} />
-        <Stack.Screen name="ItemDetail" component={ItemDetailScreen} />
-        <Stack.Screen name="ItemList" component={ItemListScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="CategoryList" component={CategoryListScreen} />
+          <Stack.Screen name="ItemDetail" component={ItemDetailScreen} />
+          <Stack.Screen name="ItemList" component={ItemListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 

bcaed889c17f6a6a367ae66ea6abd31514668429
Oscar Chan <oscarchan@asl.com.hk>
2022-04-26 11:59:33 +0800

    refactor: prettier

--- src/screens/ItemDetailScreen.tsx
+++ src/screens/ItemDetailScreen.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const ItemDetailScreen = () => {
   return (
     <View>
       <Text>ItemDetailScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ItemDetailScreen
+export default ItemDetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/ItemListScreen.tsx
+++ src/screens/ItemListScreen.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const ItemListScreen = () => {
   return (
     <View>
       <Text>ItemListScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ItemListScreen
+export default ItemListScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

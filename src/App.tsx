29bb75e58e1b445c929d7dd76d3fb5ec53f041e6
Oscar Chan <oscarchan@asl.com.hk>
2022-04-27 10:27:44 +0800

    refactor: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 // navigation
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // screens
 import LoginScreen from './screens/LoginScreen';
@@ -12,7 +12,7 @@ import ItemListScreen from './screens/ItemListScreen';
 
 // redux
 import store from './store';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 export type AppStackParamList = {
   Login: undefined;
@@ -27,7 +27,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="CategoryList">
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="CategoryList" component={CategoryListScreen} />
           <Stack.Screen name="ItemDetail" component={ItemDetailScreen} />

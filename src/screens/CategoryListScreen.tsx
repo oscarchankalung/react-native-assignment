3f3f2493cd816821f5d3e5a0bb317c25b7757ab4
Oscar Chan <oscarchan@asl.com.hk>
2022-04-27 10:30:17 +0800

    feature: try fetching article

--- src/screens/CategoryListScreen.tsx
+++ src/screens/CategoryListScreen.tsx
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useAppDispatch, useAppSelector } from '../hooks/useStore';
 
 const CategoryListScreen = () => {
+  const categories = useAppSelector(state => state.article.categories);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    // dispatch({ type: 'article/getCategories' });
+    dispatch({ type: 'article/getItems', payload: 'healthcare' });
+  }, []);
+
   return (
     <View>
       <Text>CategoryListScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CategoryListScreen
+export default CategoryListScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

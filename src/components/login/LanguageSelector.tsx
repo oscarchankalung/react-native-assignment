aeddfd2d7750bd59fb1a6ecbf0331b0ceec7e6a1
Oscar Chan <oscarchan@asl.com.hk>
2022-05-10 15:59:21 +0800

    feat: add LanguageSelector

---
+++ src/components/login/LanguageSelector.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+// translation
+import { useTranslation } from 'react-i18next';
+import { LANGUAGES } from '../../constants/Languages';
+
+const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+
+  const renderedLanguageButtons = Object.entries(LANGUAGES).map(
+    ([key, language]) => {
+      const changeLanguage = () => i18n.changeLanguage(language.i18n);
+      const selected = i18n.language === language.i18n;
+      const textStyle = selected ? styles.selectedText : styles.text;
+
+      return (
+        <TouchableOpacity
+          key={key}
+          style={styles.button}
+          onPress={changeLanguage}>
+          <Text style={textStyle}>{language.label}</Text>
+        </TouchableOpacity>
+      );
+    },
+  );
+
+  return <View style={styles.row}>{renderedLanguageButtons}</View>;
+};
+
+export default LanguageSelector;
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 16,
+    // backgroundColor: 'pink',
+  },
+  button: {
+    paddingVertical: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    // backgroundColor: 'orange',
+  },
+  text: {
+    fontSize: 18,
+    color: 'black',
+  },
+  selectedText: {
+    fontSize: 18,
+    color: 'red',
+  },
+});
